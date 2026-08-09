568599965cddeae60c8b283a4bf7eb9824d96aee
Laminate32 <enotik.cool18@gmail.com>
2026-08-09 21:47:25 +0300

    fix: fixed mocks of repositories

--- Streetcode/Streetcode.XUnitTest/MediatRTests/News/GetNewsByIdHandlerTests.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/News/GetNewsByIdHandlerTests.cs
@@ -15,7 +15,7 @@ namespace Streetcode.XUnitTest.MediatRTests.News.GetById;
 
 public class GetNewsByIdHandlerTests
 {
-    private readonly Mock<IRepositoryWrapper> _repositoryMock = new Mock<IRepositoryWrapper> { DefaultValue = DefaultValue.Mock };
+    private readonly Mock<IRepositoryWrapper> _repositoryMock = new();
     private readonly Mock<IMapper> _mapperMock = new();
     private readonly Mock<IBlobService> _blobServiceMock = new();
     private readonly Mock<ILoggerService> _loggerMock = new();
