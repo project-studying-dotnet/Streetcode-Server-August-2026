ee0c62a4e4fe75903ae3218b0d4a14cdcc0bee11
Loki22978964 <lokim3059@gmail.com>
2026-08-14 10:17:09 +0300

    feat(team): implement specification pattern for team queries
    - Add GetAllTeamSpecification and GetAllMainTeamSpecification
    - Update GetAllTeamHandler and GetAllMainTeamHandler to use ListAsync

---
+++ Streetcode/Streetcode.DAL/Specifications/Team/GetAllTeamSpecification.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.Specification;
+using Streetcode.DAL.Entities.Team;
+
+namespace Streetcode.DAL.Specifications.Team
+{
+    public class GetAllTeamSpecification : Specification<TeamMember>
+    {
+        public GetAllTeamSpecification()
+        {
+            Query
+                .Include(x => x.Positions)
+                .Include(x => x.TeamMemberLinks);
+        }
+    }
+}
