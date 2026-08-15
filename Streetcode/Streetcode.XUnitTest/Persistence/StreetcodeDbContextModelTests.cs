00b7fcebe2ae5872050d9bb987ca3975acd5d69b
emil720a1 <emil720apro@gmail.com>
2026-08-15 14:39:15 +0300

    fix: correct EF configuration and add model tests

---
+++ Streetcode/Streetcode.XUnitTest/Persistence/StreetcodeDbContextModelTests.cs
@@ -0,0 +1,63 @@
+using Microsoft.EntityFrameworkCore;
+using Streetcode.DAL.Entities.Partners;
+using Streetcode.DAL.Entities.Streetcode;
+using Streetcode.DAL.Persistence;
+using Xunit;
+
+namespace Streetcode.XUnitTest.Persistence;
+
+public class StreetcodeDbContextModelTests
+{
+    [Fact]
+    public void Model_WhenBuilt_ShouldConfigureRelatedFigureDeleteBehaviors()
+    {
+        var options = new DbContextOptionsBuilder<StreetcodeDbContext>()
+            .UseSqlServer(
+                "Server=localhost;Database=StreetcodeModelTests;" +
+                "Trusted_Connection=True;TrustServerCertificate=True;")
+            .Options;
+
+        using var context = new StreetcodeDbContext(options);
+
+        var entityType = context.Model.FindEntityType(typeof(RelatedFigure));
+
+        Assert.NotNull(entityType);
+
+        var foreignKeys = entityType.GetForeignKeys();
+
+        var observerForeignKey = foreignKeys.Single(
+            foreignKey => foreignKey.Properties.Any(
+                property => property.Name == nameof(RelatedFigure.ObserverId)));
+
+        var targetForeignKey = foreignKeys.Single(
+            foreignKey => foreignKey.Properties.Any(
+                property => property.Name == nameof(RelatedFigure.TargetId)));
+
+        Assert.Equal(DeleteBehavior.Restrict, observerForeignKey.DeleteBehavior);
+        Assert.Equal(DeleteBehavior.Cascade, targetForeignKey.DeleteBehavior);
+    }
+    
+    [Fact]
+    public void Model_WhenBuilt_ShouldConfigurePartnerDefaultValueAsBooleanFalse()
+    {
+        var options = new DbContextOptionsBuilder<StreetcodeDbContext>()
+            .UseSqlServer(
+                "Server=localhost;Database=StreetcodeModelTests;" +
+                "Trusted_Connection=True;TrustServerCertificate=True;")
+            .Options;
+
+        using var context = new StreetcodeDbContext(options);
+
+        var entityType = context.Model.FindEntityType(typeof(Partner));
+
+        Assert.NotNull(entityType);
+
+        var property = entityType.FindProperty(nameof(Partner.IsKeyPartner));
+
+        Assert.NotNull(property);
+
+        var defaultValue = Assert.IsType<bool>(property.GetDefaultValue());
+
+        Assert.False(defaultValue);
+    }
+}
