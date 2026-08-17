0c885bd42f3fe62a39f8b18683b77eac439e50b7
Loki22978964 <lokim3059@gmail.com>
2026-08-17 10:06:01 +0300

    Add Index field to Fact entity, DTO and create EF Core migration

--- Streetcode/Streetcode.BLL/DTO/Streetcode/TextContent/Fact/FactDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Streetcode/TextContent/Fact/FactDTO.cs
@@ -3,6 +3,7 @@ namespace Streetcode.BLL.DTO.Streetcode.TextContent.Fact;
 public class FactDto
 {
     public int Id { get; set; }
+    public int Index { get; set; }
     public string Title { get; set; }
     public int ImageId { get; set; }
     public string FactContent { get; set; }
