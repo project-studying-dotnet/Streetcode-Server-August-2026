50c1481e243b74a0b6eccbbf280861c2889abac6
emil720a1 <emil720apro@gmail.com>
2026-08-26 10:34:44 +0300

    feat(identity): add refresh token lifecycle

--- Streetcode.Identity/src/Streetcode.Identity.WebApi/Program.cs
+++ Streetcode.Identity/src/Streetcode.Identity.WebApi/Program.cs
@@ -3,6 +3,7 @@ using Streetcode.Identity.Application;
 using Streetcode.Identity.Infrastructure;
 using Streetcode.Identity.Infrastructure.Messaging.Kafka;
 using Streetcode.Identity.Infrastructure.Persistence;
+using Streetcode.Identity.Infrastructure.Security;
 using Streetcode.Identity.WebApi.ExceptionHandling;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +22,7 @@ if (string.IsNullOrWhiteSpace(connectionString))
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(connectionString);
+builder.Services.AddRefreshTokenServices(builder.Configuration);
 builder.Services.AddKafkaMessaging(builder.Configuration);
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
