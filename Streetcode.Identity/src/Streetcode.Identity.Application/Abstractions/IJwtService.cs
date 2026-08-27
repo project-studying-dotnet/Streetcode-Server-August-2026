e4f707bc4bae48468b49b3226ca57341ae74654e
Loki22978964 <lokim3059@gmail.com>
2026-08-27 15:18:41 +0300

    fast fix

---
+++ Streetcode.Identity/src/Streetcode.Identity.Application/Abstractions/IJwtService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Streetcode.Identity.Application.Abstractions
+{
+    public interface IJwtService
+    {
+        AuthTokenResult GenerateToken(int userId, string email, IEnumerable<string> roles);
+    }
+}
---
+++ Streetcode.Identity/src/Streetcode.Identity.Infrastructure/Identity/Jwt/JwtService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using Streetcode.Identity.Application.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace Streetcode.Identity.Infrastructure.Identity.Jwt
+{
+    public class JwtService : IJwtService
+    {
+        private readonly JwtOptions _jwtOptions;
+        private readonly JsonWebTokenHandler _tokenHandler;
+
+        public JwtService(IOptions<JwtOptions> jwtOptions)
+        {
+            _jwtOptions = jwtOptions.Value;
+            _tokenHandler = new JsonWebTokenHandler();
+        }
+
+        public AuthTokenResult GenerateToken(int userId, string email, IEnumerable<string> roles)
+        {
+            string secretKey = _jwtOptions.SecretKey;
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var now = DateTime.UtcNow;
+            var expires = now.AddMinutes(_jwtOptions.LifetimeInMinutes);
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, email),
+            };
+
+            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+            var tokenDescription = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = expires,
+                SigningCredentials = credentials,
+                Issuer = _jwtOptions.Issuer,
+                Audience = _jwtOptions.Audience,
+                IssuedAt = now
+            };
+
+            string token = _tokenHandler.CreateToken(tokenDescription);
+
+            return new AuthTokenResult(token, expires);
+        }
+    }
+}
