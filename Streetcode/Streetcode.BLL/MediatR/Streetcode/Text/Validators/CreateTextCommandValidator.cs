96722b1900c6c7ec92f02e1c23f947649584ad28
Eduard Yevchenko <82526715+Exlizardium@users.noreply.github.com>
2026-08-31 23:38:44 +0300

    Add validator and validator tests for text creation

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Text/Validators/CreateTextCommandValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using Streetcode.BLL.MediatR.Validators;
+using Streetcode.BLL.MediatR.Streetcode.Text.Create;
+using TextEntity = Streetcode.DAL.Entities.Streetcode.TextContent.Text;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Text.Validators;
+public sealed class CreateTextCommandValidator : AbstractValidator<CreateTextCommand>
+{
+    public CreateTextCommandValidator()
+    {
+        RuleFor(command => command.TextCreateDto.TextContent)
+            .NotEmpty()
+            .WithMessage("Text content is required.")
+            .MustNotExceedLength(
+                TextEntity.TextContentMaxLength,
+                "Text content");
+
+        RuleFor(command => command.TextCreateDto.Title)
+            .NotEmpty()
+            .WithMessage("Title is required.")
+            .MustNotExceedLength(
+                300,
+                "Title");
+
+        RuleFor(command => command.TextCreateDto.AdditionalText)
+            .MustNotExceedLength(
+                500,
+                "Additional text");
+
+        RuleFor(command => command.TextCreateDto.StreetcodeId)
+            .GreaterThan(0)
+            .WithMessage("StreetcodeId must be a valid positive identifier.");
+    }
+}
