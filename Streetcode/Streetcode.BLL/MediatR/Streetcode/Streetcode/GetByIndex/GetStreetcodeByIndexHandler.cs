ac1de3aa839c7502ce10faf3129ff5fc9c7839c3
Loki22978964 <lokim3059@gmail.com>
2026-09-14 20:52:44 +0300

    fix: test fix

--- Streetcode/Streetcode.BLL/MediatR/Streetcode/Streetcode/GetByIndex/GetStreetcodeByIndexHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Streetcode/GetByIndex/GetStreetcodeByIndexHandler.cs
@@ -6,7 +6,6 @@ using Streetcode.BLL.DTO.Streetcode;
 using Streetcode.BLL.Interfaces.CacheService;
 using Streetcode.BLL.Interfaces.Logging;
 using Streetcode.DAL.Repositories.Interfaces.Base;
-using Streetcode.DAL.Repositories.Realizations.Base;
 
 namespace Streetcode.BLL.MediatR.Streetcode.Streetcode.GetByIndex;
 
--- Streetcode/Streetcode.BLL/Services/CacheService/NoOpCacheService.cs
+++ Streetcode/Streetcode.BLL/Services/CacheService/NoOpCacheService.cs
@@ -1,6 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Streetcode.BLL.Interfaces.CacheService;
 
 namespace Streetcode.BLL.Services.CacheService;
