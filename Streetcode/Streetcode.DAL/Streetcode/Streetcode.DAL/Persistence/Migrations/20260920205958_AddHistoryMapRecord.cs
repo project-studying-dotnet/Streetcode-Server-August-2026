656039e618aff600c82ee6221999cb1224597ad2
M1R4MI <vadym3110mh@gmail.com>
2026-09-21 00:02:24 +0300

    Updated migrations for database

---
+++ Streetcode/Streetcode.DAL/Streetcode/Streetcode.DAL/Persistence/Migrations/20260920205958_AddHistoryMapRecord.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Streetcode.DAL.Streetcode.Streetcode.DAL.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHistoryMapRecord : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_history_map_records_StreetcodeId",
+                schema: "streetcode",
+                table: "history_map_records");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_history_map_records_StreetcodeId_PhysicalStreetcodeNumber",
+                schema: "streetcode",
+                table: "history_map_records",
+                columns: new[] { "StreetcodeId", "PhysicalStreetcodeNumber" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_history_map_records_StreetcodeId_PhysicalStreetcodeNumber",
+                schema: "streetcode",
+                table: "history_map_records");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_history_map_records_StreetcodeId",
+                schema: "streetcode",
+                table: "history_map_records",
+                column: "StreetcodeId");
+        }
+    }
+}
